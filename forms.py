2117663ea276a8c20565d992b063c3b98b340c8e
M-Shahzaman <shahzamaneng3@gmail.com>
2022-09-07 12:01:35 +0500

    initial commit

---
+++ forms.py
@@ -0,0 +1,37 @@
+from flask import app
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+from flask_gravatar import Gravatar
+
+
+# WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    author = StringField("Author", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign-Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+class CommentForm(FlaskForm):
+    comment = CKEditorField("comment", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+
